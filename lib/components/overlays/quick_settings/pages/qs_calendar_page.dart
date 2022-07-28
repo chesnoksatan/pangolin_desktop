1eafd67c5f29c771b84e98b30b30669021f753b5
chesnoksatan <chestwins@gmail.com>
2022-07-28 22:05:57 +0300

    QuickCalendar
    add syncfusion_flutter_calendar in pubspec

--- lib/components/overlays/quick_settings/pages/qs_calendar_page.dart
+++ lib/components/overlays/quick_settings/pages/qs_calendar_page.dart
@@ -1,6 +1,8 @@
+import 'package:intl/intl.dart';
 import 'package:pangolin/components/overlays/quick_settings/widgets/qs_titlebar.dart';
 import 'package:pangolin/utils/extensions/extensions.dart';
 import 'package:pangolin/utils/other/date_time_manager.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class QsCalendarPage extends StatelessWidget {
   const QsCalendarPage({super.key});
@@ -13,22 +15,30 @@ class QsCalendarPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Row(
-          children: [
-            const SizedBox(width: 200),
-            ValueListenableBuilder(
-              valueListenable: DateTimeManager.getDateNotifier()!,
-              builder: (BuildContext context, String date, child) {
-                return ValueListenableBuilder(
-                  valueListenable: DateTimeManager.getTimeNotifier()!,
-                  builder: (BuildContext context, String time, child) {
-                    return Text("$date - $time");
-                  },
-                );
-              },
-            ),
-          ],
+        child: ValueListenableBuilder(
+          valueListenable: DateTimeManager.getDateNotifier()!,
+          builder: (context, String value, child) {
+            return Calendar();
+          },
         ),
+      ),
+    );
+  }
+}
+
+class Calendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return SfCalendar(
+      view: CalendarView.month,
+      headerDateFormat: "EE, MMMM d, yyyy",
+      showNavigationArrow: true,
+      selectionDecoration: BoxDecoration(
+        color: theme.colorScheme.secondary.withOpacity(0.2),
+      ),
+      headerStyle: const CalendarHeaderStyle(
+        textAlign: TextAlign.center,
       ),
     );
   }
